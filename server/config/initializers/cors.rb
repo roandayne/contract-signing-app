7281f1ed6d07b9f89b51de6b2c0843b960a73ccc
roandayne <roandaynedenaerys@gmail.com>
2025-03-28 00:34:21 +0800

    Update cors

--- server/config/initializers/cors.rb
+++ server/config/initializers/cors.rb
@@ -1,10 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV['CLIENT_URL'] || '*'
+    origins ENV['CLIENT_URL']
     resource '*',
       headers: :any,
       methods: %i[get post put patch delete options head],
-      credentials: true,
-      expose: ['Authorization']
+      credentials: true
   end
 end
