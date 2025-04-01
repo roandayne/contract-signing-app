9f154b3007721d393e0de9e8c74b25fe604e82d0
roandayne <roandaynedenaerys@gmail.com>
2025-04-01 12:55:04 +0800

    Fix cors error in prod and update packages

--- server/config/initializers/cors.rb
+++ server/config/initializers/cors.rb
@@ -1,9 +1,15 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV['CLIENT_URL']
+    if Rails.env.development?
+      origins 'http://localhost:5173'
+    else
+      origins 'https://ceo-sidekicks-contract-signing.netlify.app'
+    end
+    
     resource '*',
       headers: :any,
-      methods: %i[get post put patch delete options head],
-      credentials: true
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true,
+      expose: ['access-token', 'expiry', 'token-type', 'uid', 'client']
   end
 end
